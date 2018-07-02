9de3bc987f8fc046cdb06784ba88c622cfc01181
Sam Byers <sam@sambyers.net>
2018-07-01 22:05:16 -0400

    linted the two examples

--- nornir_test1.py
+++ nornir_test1.py
@@ -1,17 +1,29 @@
 from nornir.core import InitNornir
-from nornir.plugins.tasks import commands, networking
+from nornir.plugins.tasks import networking
 from nornir.plugins.functions.text import print_result
 
+
 def device_audit(task):
-    task.run(task=networking.napalm_get, name="Device Interfaces", getters=["interfaces"])
-    task.run(task=networking.napalm_get, name="Device Environment", getters=["environment"])
-    task.run(task=networking.napalm_get, name="Device Users", getters=["users"])
+
+    task.run(
+        task=networking.napalm_get,
+        name="Device Interfaces",
+        getters=["interfaces"])
+    task.run(
+        task=networking.napalm_get,
+        name="Device Environment",
+        getters=["environment"])
+    task.run(
+        task=networking.napalm_get,
+        name="Device Users",
+        getters=["users"])
+
 
 host_inv = InitNornir(config_file="config.yaml")
 
 test_hosts = host_inv.filter(site="chiba", type="network_device")
 
-napalm_result = test_hosts.run(task=networking.napalm_get,getters=["facts"])
+napalm_result = test_hosts.run(task=networking.napalm_get, getters=["facts"])
 print_result(napalm_result)
 
 task_result = test_hosts.run(task=device_audit)
--- nornir_test2.py
+++ nornir_test2.py
@@ -1,39 +1,50 @@
 from nornir.core import InitNornir
 from nornir.plugins.tasks import networking, text
 from nornir.plugins.functions.text import print_title, print_result
-from nornir.plugins.tasks.files import write_file
+
 
 def basic_configuration(task):
     # Transform inventory data to configuration via a template file
-    r = task.run(task=text.template_file,
-                 name="Building basic Configuration",
-                 template="basic.j2",
-                 path=f"templates/{task.host.nos}")
+
+    r = task.run(
+        task=text.template_file,
+        name="Building basic Configuration",
+        template="basic.j2",
+        path=f"templates/{task.host.nos}")
 
     # Save the compiled configuration into a host variable
     task.host["config"] = r.result
 
     # Deploy that configuration to the device using NAPALM
-    task.run(task=networking.napalm_configure,
-             name="Loading Configuration on the device",
-             replace=False, # False = merge config, True = replace config
-             configuration=task.host["config"])
+    task.run(
+        task=networking.napalm_configure,
+        name="Loading Configuration on the device",
+        replace=False,   # False = merge config, True = replace config
+        configuration=task.host["config"])
+
 
 def enable_scp_ios(task):
-    r = task.run(task=networking.netmiko_send_command,
-                name="Loading SCP Configuration on IOS device",
-                command_string="configure terminal\n\nip scp server enable\n\nend\n\n")
+    r = task.run(
+        task=networking.netmiko_send_command,
+        name="Loading SCP Configuration on IOS device",
+        command_string="configure terminal\n\nip scp server enable\n\nend\n\n")
     return r
 
-nn = InitNornir(config_file="config.yaml", dry_run=False)
-# Filter the inventory based on some attributes of devices. In this case the 'site' and 'type.'
-test_site_devices = nn.filter(site="chiba", type="network_device")
-ios_test_site_devices = nn.filter(site="chiba", nornir_nos="ios") # Filter based on site and OS.
 
-#Get configuration from IOS devices
-ios_config_result = ios_test_site_devices.run(task=networking.napalm_get,
-                                                        getters=["config"])
-# Check the running configuration of IOS devices and if not configured for SCP, configure it.
+nn = InitNornir(config_file="config.yaml", dry_run=False)
+# Filter the inventory based on some attributes of devices.
+# In this case the 'site' and 'type.'
+test_site_devices = nn.filter(site="chiba", type="network_device")
+# Filter IOS devices
+ios_test_site_devices = nn.filter(site="chiba", nornir_nos="ios")
+
+# Get configuration from IOS devices
+ios_config_result = ios_test_site_devices.run(
+    task=networking.napalm_get,
+    getters=["config"])
+
+# Check the running configuration of IOS devices.
+# If not configured for SCP, configure it.
 for h, r in ios_config_result.items():
     if "ip scp server enable" not in r.result["config"]["running"]:
         print_title("Playbook to enable SCP on IOS devices at site 'test.'")
